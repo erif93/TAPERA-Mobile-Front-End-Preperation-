ade4488683500af7590bd37739576d6091298833
unknown <linglung5566@gmail.com>
2020-05-28 16:38:31 +0700

    update login page

---
+++ lib/model/post_result_model.dart
@@ -0,0 +1,33 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class PostResult {
+  String message;
+  String sessionId;
+  String sessionUser;
+  String status;
+
+  PostResult({this.message, this.sessionId, this.sessionUser, this.status});
+  
+  factory PostResult.createPostResult(Map<String, dynamic> object){
+    return PostResult(
+      message: object['message'],
+      sessionId: object['sessionId'],
+      sessionUser: object['sessionUser'],
+      status: object['status']
+    );
+  }
+  static Future<PostResult> connectToAPI(String username, password) async
+  {
+    String credentials = username+':'+password;
+    Codec<String, String> stringaToBase64 = utf8.fuse(base64);
+    String encoded = 'Basic ' + stringaToBase64.encode(credentials);
+    print(encoded);
+
+    String url = 'http://35.198.233.87:8080/login'; 
+    //String url = 'https://reqres.in/api/users'; 
+    var result = await http.post(url, headers: {'Authorization':encoded});
+    var jsonObject = json.decode(result.body);
+    return PostResult.createPostResult(jsonObject);
+  }
+}
---
+++ lib/page/login_page.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'package:belajar_flutter/model/post_result_model.dart';
+import 'package:belajar_flutter/page/home_page.dart';
+
+class Login extends StatefulWidget {
+  @override
+  _LoginState createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  PostResult postResult = null;
+  TextEditingController usernameController = new TextEditingController();
+  TextEditingController passwordController = new TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+    appBar: AppBar(
+      title: Text("Login Form "),
+      centerTitle: true,
+      backgroundColor: Colors.blue,
+    ),
+    body: Container(
+      padding: const EdgeInsets.all(32),
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                TextField(
+                  controller: usernameController,
+                  decoration: InputDecoration(
+                    labelText: 'username',
+                  ),
+                ),
+                TextField(
+                  controller: passwordController,
+                  decoration: InputDecoration(
+                    labelText: 'Password',
+                  ),
+                  obscureText: true,
+                ),
+               // Text((postResult != null) ? postResult.sessionUser : 'tidak ada data'),
+                RaisedButton(
+                  color: Colors.blue,
+                  onPressed: (){
+                    print(usernameController.text);
+                    print(passwordController.text);
+                    PostResult.connectToAPI(usernameController.text, passwordController.text).then((value) {
+                      postResult = value;
+                      setState(() {});
+
+                      Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => HomePage(text: postResult.sessionUser,),
+                    ),);
+                      
+                    });
+                  },
+                  child: Text(
+                    'Login',
+                    style: TextStyle(
+                      color: Colors.white
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    ), 
+  );
+  }
+}
